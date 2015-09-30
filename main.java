d6dfa1b0aa0366f67bb1316f5b797116f7c394db
jaehoochoi <jaehoochoi@wireless-10-145-102-0.public.utexas.edu>
2015-09-29 19:22:30 -0500

    second

--- main.java
+++ main.java
@@ -22,8 +22,8 @@ public class Main {
 	 * 
 	 */
 	private static class Node {
-		ArrayList<String> nextword = new ArrayList<String>();
-		boolean visted = false;
+		private ArrayList<String> nextword = new ArrayList<String>();
+		private boolean visted = false;
 	}
 	
 	/* public static void main(String[] args)
@@ -53,6 +53,45 @@ public class Main {
 
 	}
 	
+	/* public static ArrayList<String> BFS(String start, String end, HashMap<String, Node> data)
+	 * find the shortest way to get the word
+	 */
+	public static ArrayList<String> BFS(String start, String end, HashMap<String, Node> data) {
+		// special case: when start and end are equal
+		if(start.equals(end)) { 
+			return null; 
+		}
+		
+		ArrayList<String> ans = new ArrayList<String>();
+		Queue<String> q = new LinkedList<String>();
+		
+		Node adjacent = data.get(start);
+		adjacent.visted = true;	// check we visited start at the beginning. 
+		
+		while(true) {
+			// add all the adjacent words of start to queue
+			for(int i = 1; i < adjacent.nextword.size(); i++) {
+				q.add(adjacent.nextword.get(i));
+			}
+			
+			// get the word from queue and get the node from hashmap
+			adjacent = data.get(q.poll());
+			adjacent.visted = true;
+			// TODO how can we access to the string in hashmap?
+			if(adjacent)
+			
+			// if queue is empty, done
+			if(q.isEmpty()) { 
+				break;
+			}
+		}
+		
+		
+		
+		
+		return null;
+	}
+	
 	/* public static ArrayList<String> getWordLadder(String start, String end)
 	 * given method
 	 */
@@ -62,9 +101,9 @@ public class Main {
 		
 		// TODO BFS algo
 		Node possibleWords = data.get(start);
-		possibleWords.visted = true;
+		// possibleWords.visted = true;
 		// TODO index
-		possibleWords.nextword.get(index);
+		// possibleWords.nextword.get(index);
 		
 		return null;
 	}
@@ -99,7 +138,6 @@ public class Main {
 			String word = iterDict.next();
 			map.put(word, saveToArrayList(word, dict));
 		}
-		
 		return map;
 	}
 	
@@ -110,13 +148,15 @@ public class Main {
 		Iterator<String> iterDict = dict.iterator();
 		Node possibleWords = new Node();
 		
+		possibleWords.nextword.add(word);
+		
 		while(iterDict.hasNext()) {
 			String nextWord = iterDict.next();
 			if(compareWords(word, nextWord)) {
+				
 				possibleWords.nextword.add(nextWord);
 			}
 		}
-		
 		return possibleWords;
 	}
 	
@@ -124,14 +164,22 @@ public class Main {
 	 * compares two strings and return true if s2 has only one different character compared to s1
 	 */
 	public static boolean compareWords(String s1, String s2) {
-		int count_of_diff_letters = 0;
+		int countOfDiffLetters = 0;
 		
 		for(int i = 0; i < 5; i++){
-			if(s1.getBytes()[i] != s2.getBytes()[i]) count_of_diff_letters++;
-			if(count_of_diff_letters > 1) return false;
+			if(s1.getBytes()[i] != s2.getBytes()[i]) {
+				countOfDiffLetters++;
+			}
+			if(countOfDiffLetters > 1) {
+				return false;
+			}
 		}
 		
-		if(count_of_diff_letters == 1) return true;
-		else return false;
+		if(countOfDiffLetters == 1) {
+			return true;
+		}
+		else { 
+			return false;
+		}
 	}
 }
